52e33071e7e8e775900c3f30bd878c2d8deaf9fb
Kevin Zoltany <kzoltany@gmail.com>
2025-12-25 20:54:50 -0800

    Added Convex

---
+++ packages/backend/src/convex/schema.ts
@@ -0,0 +1,25 @@
+import { v } from "convex/values";
+import { defineSchema, defineTable } from "convex/server";
+
+export default defineSchema({
+    bans: defineTable({
+        userId: v.string(),
+        username: v.string(),
+        globalName: v.string(),
+        tag: v.string(),
+        discriminator: v.string(),
+        bot: v.boolean(),
+        accountCreated: v.number(),
+        joinedServer: v.number(),
+        avatar: v.string(),
+        roles: v.array(v.string()),
+        nickname: v.optional(v.string()),
+        banTimestamp: v.number(),
+        reason: v.string(),
+        protectedRoleId: v.string(),
+        guildId: v.string(),
+    })
+    .index("byUserId", ["userId"])
+    .index("byGuildId", ["guildId"])
+    .index("byTime", ["banTimestamp"]),
+});

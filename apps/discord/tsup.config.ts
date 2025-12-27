4f5d5e289e95214788f4e9ffa33162c49e765e32
Kevin Zoltany <kzoltany@gmail.com>
2025-12-27 10:53:08 -0800

    Added external to tsup

--- apps/discord/tsup.config.ts
+++ apps/discord/tsup.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ['src/index.ts'],
-    format: ['esm'],
-    target: 'node20',
-    outDir: 'dist',
-    clean: true,
-    sourcemap: true,
-    minify: false,
-    treeshake: true
-})
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  target: "node20",
+  outDir: "dist",
+  clean: true,
+  sourcemap: true,
+  minify: true,
+  treeshake: true,
+  noExternal: ["@rbbp/backend"],
+});
